712f145c82917fa7575f7daeb60245db44c1d467
capliens <qudjchoizn@gmail.com>
2024-04-12 10:17:34 +0900

    도서 수정

--- src/main/java/kr/kh/team4/model/vo/book/UnderVO.java
+++ src/main/java/kr/kh/team4/model/vo/book/UnderVO.java
@@ -10,4 +10,32 @@ public class UnderVO {
 	private String un_name;
 	private int un_code;
 	private int un_up_num;
+	
+	public UnderVO(int un_code) {
+		this.un_code = un_code;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UnderVO other = (UnderVO) obj;
+		if (un_code != other.un_code)
+			return false;
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + un_code;
+		return result;
+	}
+	
+	
 }
